fa64452f82e2a96de6f1d12b078abab55014c014
cristianvaz <cristianvaz81993@gmail.com>
2022-06-24 01:10:04 +0200

    bitra: LineagePlus bringup

--- lineage_bitra.mk
+++ lineage_bitra.mk
@@ -18,9 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
-ARROW_GAPPS := true
+# Inherit some common LineagePlus stuff
+$(call inherit-product, vendor/lineage/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
@@ -28,11 +27,20 @@ TARGET_INCLUDE_PIXEL_CHARGER := true
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_bitra
+PRODUCT_NAME := lineage_bitra
 PRODUCT_DEVICE := bitra
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3370
 PRODUCT_MANUFACTURER := realme
+
+# LineagePlus flags
+BUILD_TYPE := OFFICIAL
+TARGET_INCLUDE_GAPPS := true
+TARGET_INCLUDE_LAWNCHAIR = true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+PLUS_MAINTAINER := cristianvaz
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
